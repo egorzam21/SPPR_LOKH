f07ec79208f0d13bc3ef80006aa9a75ef2238e82
egorzam21 <147714238+egorzam21@users.noreply.github.com>
2025-11-20 20:53:59 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from tinkoff.invest import Client
-
-token = "t.sWXWh2h48nFyH3cFr886QxrA9xNOHh2Sy6ULpJydAb0f_7_HbQqfUaRbQ6BmGI6cMRNT6fcC4VmRYW7NmzOseg"
-
-with Client(token) as client:
-    instruments = client.instruments.futures()
-    for f in instruments.instruments:
-        if "GLDRUBF" in f.ticker:
-            print(f.ticker, f.figi, f.name)
